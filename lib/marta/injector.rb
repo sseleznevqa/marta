dbc510e6916b3293af6cf0680fef487775b64667
Sergei Seleznev <s_seleznev_qa@hotmail.com>
2017-07-19 12:20:56 +0300

    More flexible injector

--- lib/marta/injector.rb
+++ lib/marta/injector.rb
@@ -16,12 +16,19 @@ module Marta
     class Syringe
 
       def initialize(engine, marta_what, title = 'Something important',
-                     old_data = Hash.new, folder = gem_libdir)
+                     old_data = Hash.new, folder = gem_libdir,
+                     custom_vars = Array.new, custom_scripts = Array.new)
         @what = marta_what
         @title = title
         @data = old_data
+        @data ||= Hash.new
         @engine = engine
         @folder = folder
+        @custom_vars = custom_vars
+        @custom_scripts = custom_scripts
+        @default_vars = [{"marta_what": "\"#{@title}\""},
+          {"old_marta_Data": @data.to_s.gsub('=>',':').gsub('nil','null')}]
+        @default_scripts = ["marta_add_data();"]
       end
 
       # "first" or "last".
@@ -38,7 +45,7 @@ module Marta
         newMartaObject.innerHTML   = '#{inner}';
         document.body.insertBefore(newMartaObject,document.body.#{where}Child);
         JS
-         @engine.execute_script script.gsub("\n",'')
+        @engine.execute_script script.gsub("\n",'')
       end
 
       # Taking a correct js file to inject
@@ -63,17 +70,38 @@ module Marta
         insert_to_page('style', style, false)
       end
 
+      # Syringe sets javascript variables
+      def set_var(var, value)
+        insert_to_page('script', "var #{var} = #{value};", false)
+      end
+
+      # Syringe runs scripts
+      def run_script(script)
+        @engine.execute_script(script)
+      end
+
+      # Syringe takes array of hashes to set lots of variables
+      def set_vars(vars_array)
+        vars_array.each do |var_hash|
+          var_hash.each_pair do |var_name, var_value|
+            set_var var_name.to_s, var_value
+          end
+        end
+      end
+
+      # Syringe can run an array of scripts
+      def all_scripts(scripts_array)
+        scripts_array.each do |script|
+          run_script script
+        end
+      end
+
       # It is never used without get_result.
       # But it can be used to show some message for user
       def actual_injection
         files_to_page
-        @data ||= Hash.new
-        insert_to_page('script', "var marta_what = \"#{@title}\"", false)
-        insert_to_page('script',
-                       "var old_marta_Data = #{@data}".gsub('=>',':').
-                                                           gsub('nil','null'),
-                       false)
-        @engine.execute_script("marta_add_data();")
+        set_vars(@default_vars + @custom_vars)
+        all_scripts(@default_scripts + @custom_scripts)
       end
 
       # Retrieving result if js var = marta_confirm_mark is true
@@ -94,8 +122,10 @@ module Marta
     end
 
     # That's how Marta is injecting and retrieving result
-    def inject(what, title = 'Something important', data = Hash.new)
-      syringe = Syringe.new(engine, what, title, data, gem_libdir)
+    def inject(what, title = 'Something important', data = Hash.new,
+                        custom_vars = Array.new, custom_scripts = Array.new)
+      syringe = Syringe.new(engine, what, title, data, gem_libdir,
+                                                 custom_vars, custom_scripts)
       syringe.actual_injection
       syringe.get_result
     end
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -10,7 +10,9 @@ describe Marta::SmartPage, :need_browser do
   before(:each) do
     @browser.goto @page_five_url
     @syring = Marta::Injector::Syringe.new(@browser, 'for_test', 'title',
-                                          'nil||data', marta_fire(:gem_libdir))
+                                          'nil||"data"', marta_fire(:gem_libdir),
+                                          ["wild_tiger": '"Joe"'],
+                                          ["document.title = wild_tiger;"])
   end
 
   # If it is failed. Are u sure that rake install was performed?
@@ -31,15 +33,23 @@ describe Marta::SmartPage, :need_browser do
     expect(@browser.text.include?("data")).to be true
   end
 
+  it 'is injecting additional vars and scripts' do
+    @syring.actual_injection
+    expect(@browser.title == "Joe").to be true
+  end
+
   it 'is injecting style' do
     # Sorry. I do not know how to check it now :)
   end
 
   it 'is injecting html and js all data and gets a result' do
-    result = marta_fire(:inject, 'for_test', 'title', 'data')
+    result = marta_fire(:inject, 'for_test', 'title', '"data"',
+                                 ["wild_tiger": '"Joe"'],
+                                 ["document.title = wild_tiger;"])
     expect(@browser.text.include?("INJECT")).to be true
     expect(@browser.text.include?("title")).to be true
     expect(@browser.text.include?("data")).to be true
+    expect(@browser.title == "Joe").to be true
     expect(result).to eq 'Done'
   end
 
@@ -48,7 +58,7 @@ describe Marta::SmartPage, :need_browser do
     expect(@browser.text.include?("not reloaded")).to be true
     expect(@browser.text.include?("Hi, there.")).to be false
     @browser.execute_script('setTimeout(function() { window.location.reload(true); }, 1500);')
-    result = marta_fire(:inject, 'for_test', 'title', 'data')
+    result = marta_fire(:inject, 'for_test', 'title', '"data"')
     expect(result).to eq 'Done'
     expect(@browser.text.include?("not reloaded")).to be false
     expect(@browser.text.include?("Hi, there.")).to be true
