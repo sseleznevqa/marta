2b3b7f5a0a2c4f8376f338e2a71055187f18f246
Sergei Seleznev <s_seleznev_qa@hotmail.com>
2017-09-06 11:01:40 +0300

    For now it is not the serer time

--- lib/marta.rb
+++ lib/marta.rb
@@ -11,11 +11,9 @@ require 'marta/json_2_class'
 require 'marta/black_magic'
 require 'marta/simple_element_finder'
 require 'marta/x_path'
-require 'marta/server'
 require "watir"
 require 'fileutils'
 require 'json'
-require 'webrick'
 
 #
 # Marta class is providing three simple methods.
@@ -32,7 +30,7 @@ module Marta
 
     include BlackMagic, XPath, SimpleElementFinder, ClassesCreation,
             PublicMethods, Dialogs, Injector, Lightning, OptionsAndPaths,
-            Json2Class, ReadWrite, UserValuePrework, Server
+            Json2Class, ReadWrite, UserValuePrework
 
     # open_page can create new instance
     def self.open_page(*args)
@@ -64,7 +62,6 @@ module Marta
   # Read more in the README
   def dance_with(browser: nil, folder: nil, learn: nil, tolerancy: nil,
                  base_url: nil, cold_timeout: nil)
-    MartaServer.new if engine.nil?
     SettingMaster.set_engine browser
     SettingMaster.set_folder folder
     SettingMaster.set_base_url base_url
