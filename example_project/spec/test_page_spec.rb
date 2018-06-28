3676e4daf82f2125a2906925aaa95bd46d3bb370
Sergei Seleznev <s_seleznev_qa@hotmail.com>
2018-06-28 09:28:43 +0300

    Iframe reinject fix

--- example_project/spec/test_page_spec.rb
+++ example_project/spec/test_page_spec.rb
@@ -4,6 +4,23 @@ describe "We will do some dummy things like" do
   before(:each) do
     @page = MyTestPage.open_page
   end
+
+  context "Angry iframe test" do
+    before(:each) do
+      @b = engine
+      dance_with browser: @page.the_iframe
+    end
+
+    after(:each) do
+      dance_with browser: @b
+    end
+
+    it "Looking into iframe" do
+      the_iframe = TheIframe.new
+      expect(the_iframe.github_link.text).to_eq "GITHUB link"
+    end
+  end
+
   it "touching every single element at the test page" do
     expect(@page.numbers[1].text).to eq "Two"
     @page.form_fill
